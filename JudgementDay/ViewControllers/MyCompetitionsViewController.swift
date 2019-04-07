ba5272b3acb04b0b7a65581c636900a81950beba
user150225 <user150225@74-80-250-219.ic2net.net>
2019-04-07 12:49:30 -0700

    Added view controllers to a few views

---
+++ JudgementDay/ViewControllers/MyCompetitionsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MyCompetitionsViewController.swift
+//  JudgementDay
+//
+//  Created by user150225 on 4/3/19.
+//  Copyright © 2019 Weber State. All rights reserved.
+//
+
+import Foundation

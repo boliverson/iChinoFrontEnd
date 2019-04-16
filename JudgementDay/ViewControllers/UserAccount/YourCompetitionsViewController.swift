e00a376612748e5430305df5d019e308c7a705ea
Rachel <Rachelmartinez101@gmail.com>
2019-04-15 20:53:38 -0700

    Added constraint and back btn to Your Competitions

---
+++ JudgementDay/ViewControllers/UserAccount/YourCompetitionsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  YourCompetitionsViewController.swift
+//  JudgementDay
+//
+//  Created by user150225 on 4/15/19.
+//  Copyright © 2019 Weber State. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class YourCompetitionsViewController: UIViewController {
+
+    @IBAction func didSelectBackBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
